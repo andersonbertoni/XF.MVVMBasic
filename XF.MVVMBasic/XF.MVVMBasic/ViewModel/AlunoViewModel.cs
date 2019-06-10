68603e3c20c31ee931a1e2719b8639a2d69d5725
andersonbertoni <andersonbertoni@gmail.com>
2019-06-10 17:32:58 -0300

    + Continuação do exercício - Início dos desafios propostos (itens 2 e 3);

--- XF.MVVMBasic/XF.MVVMBasic/ViewModel/AlunoViewModel.cs
+++ XF.MVVMBasic/XF.MVVMBasic/ViewModel/AlunoViewModel.cs
@@ -13,26 +13,46 @@ namespace XF.MVVMBasic.ViewModel
         public string Nome { get; set; }
         public string Email { get; set; }
 
-        #endregion
+        public static List<Aluno> ListaAlunos { get; set; }
 
+        #endregion
+                
         public AlunoViewModel(Aluno aluno)
         {
             this.RM = aluno.RM;
             this.Nome = aluno.Nome;
             this.Email = aluno.Email;
+            
         }
 
-        public static Aluno GetAluno()
+        public static List<Aluno> GetAluno()
         {
-            var aluno = new Aluno()
-            {
-                Id = Guid.NewGuid(),
-                RM = "542621",
-                Nome = "Anderson Silva",
-                Email = "anderson@ufc.com"
-            };
+            //var aluno = new Aluno()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    RM = "542621",
+            //    Nome = "Anderson Silva",
+            //    Email = "anderson@ufc.com"
+            //};
 
-            return aluno;
+            return ListaAlunos;
+        }
+
+        public static bool InsertAluno(Aluno aluno)
+        {
+            try
+            {
+                if (ListaAlunos == null)
+                    ListaAlunos = new List<Aluno>();
+
+                ListaAlunos.Add(aluno);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
